c8185b56f15299ee798423650cd08e260fd014ff
zaki <cnzakii@gmail.com>
2023-08-30 11:18:52 +0800

    commit notability

---
+++ default/notability.js
@@ -0,0 +1,39 @@
+/*************************************
+
+ 项目名称：Notability
+
+ [rewrite_local]
+ ^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://raw.githubusercontent.com/cnzakii/Rewrite/main/default/notability.js
+
+ [mitm]
+ hostname = notability.com
+
+ *************************************/
+var result = JSON.parse($response.body);
+
+result = {
+    "data" : {
+        "processAppleReceipt" : {
+            "error" : 0,
+            "subscription" : {
+                "productId" : "com.gingerlabs.Notability.premium_subscription",
+                "originalTransactionId" : "570001184068302",
+                "tier" : "premium",
+                "refundedDate" : null,
+                "refundedReason" : null,
+                "isInBillingRetryPeriod" : false,
+                "expirationDate" : "2099-09-09T09:09:09.000Z",
+                "gracePeriodExpiresAt" : null,
+                "overDeviceLimit" : false,
+                "expirationIntent" : null,
+                "__typename" : "AppStoreSubscription",
+                "user" : null,
+                "status" : "canceled",
+                "originalPurchaseDate" : "2022-09-09T09:09:09.000Z"
+            },
+            "__typename" : "SubscriptionResult"
+        }
+    }
+};
+
+$done({body : JSON.stringify(result)});
